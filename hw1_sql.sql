29bea28be16f9dcbd6f9b4636cfd8533f7f02777
daryarazh <mur_ena@mail.ru>
2023-02-10 18:50:46 +0300

    add hw3

--- hw1_sql.sql
+++ hw1_sql.sql
@@ -1,29 +1,29 @@
-1. Найдите номер модели, скорость и размер жесткого диска для всех ПК стоимостью менее 500 дол. Вывести: model, speed и hd
+-- 1. Найдите номер модели, скорость и размер жесткого диска для всех ПК стоимостью менее 500 дол. Вывести: model, speed и hd
 select model, speed, hd from PC
 where price < 500
 
-2. Найдите производителей принтеров. Вывести: maker
+-- 2. Найдите производителей принтеров. Вывести: maker
 select distinct maker from product
 where type = 'printer'
 
-3. Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.
+-- 3. Найдите номер модели, объем памяти и размеры экранов ПК-блокнотов, цена которых превышает 1000 дол.
 select model, ram, screen from laptop
 where price > 1000
 
-4. Найдите все записи таблицы Printer для цветных принтеров.
+-- 4. Найдите все записи таблицы Printer для цветных принтеров.
 select * from printer
 where color = 'y'
 
-5. Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол
+-- 5. Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол
 select model, speed, hd from pc
 where cd in ('12x', '24x') and price < 600
 
-6. Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость
+-- 6. Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость
 select distinct product.maker, laptop.speed from laptop
 join product on product.model = laptop.model
 where laptop.hd >= 10
 
-7. Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква)
+-- 7. Найдите номера моделей и цены всех имеющихся в продаже продуктов (любого типа) производителя B (латинская буква)
 select product.model, pc.price from pc
 join product on product.model = pc.model
 where product.maker = 'B'
@@ -36,84 +36,84 @@ select product.model, printer.price from printer
 join product on product.model = printer.model
 where product.maker = 'B'
 
-8. Найдите производителя, выпускающего ПК, но не ПК-блокноты
+-- 8. Найдите производителя, выпускающего ПК, но не ПК-блокноты
 select maker from product
 where type = 'pc'
 except select maker from product
 where type = 'laptop'
 
-9. Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker
+-- 9. Найдите производителей ПК с процессором не менее 450 Мгц. Вывести: Maker
 select distinct maker from product
 join pc on product.model = pc.model
 where pc.speed >= 450
 
-10. Найдите модели принтеров, имеющих самую высокую цену. Вывести: model, price
+-- 10. Найдите модели принтеров, имеющих самую высокую цену. Вывести: model, price
 select model, price from printer
 where price = (select max(price) from printer)
 
-11. Найдите среднюю скорость ПК
+-- 11. Найдите среднюю скорость ПК
 select avg(speed) from pc
 
-12. Найдите среднюю скорость ПК-блокнотов, цена которых превышает 1000 дол.
+-- 12. Найдите среднюю скорость ПК-блокнотов, цена которых превышает 1000 дол.
 select avg(speed) from laptop
 where price > 1000
 
-13. Найдите среднюю скорость ПК, выпущенных производителем A.
+-- 13. Найдите среднюю скорость ПК, выпущенных производителем A.
 select avg(speed) from pc
 join product on product.model = pc.model
 where product.maker = 'A'
 
-14. Найдите класс, имя и страну для кораблей из таблицы Ships, имеющих не менее 10 орудий.
+-- 14. Найдите класс, имя и страну для кораблей из таблицы Ships, имеющих не менее 10 орудий.
 select ships.class, name, classes.country from ships
 join classes on classes.class = ships.class
 where classes.numGuns >= 10
 
-15. Найдите размеры жестких дисков, совпадающих у двух и более PC. Вывести: HD
+-- 15. Найдите размеры жестких дисков, совпадающих у двух и более PC. Вывести: HD
 select hd from pc
 group by hd
 having count(hd) > 1
 
-16. Найдите пары моделей PC, имеющих одинаковые скорость и RAM. В результате каждая пара указывается только один раз, т.е. (i,j), но не (j,i), Порядок вывода: модель с большим номером, модель с меньшим номером, скорость и RAM.
+-- 16. Найдите пары моделей PC, имеющих одинаковые скорость и RAM. В результате каждая пара указывается только один раз, т.е. (i,j), но не (j,i), Порядок вывода: модель с большим номером, модель с меньшим номером, скорость и RAM.
 select distinct p1.model, p2.model, p1.speed, p1.ram from pc p1
 join pc p2 on p1.speed = p2.speed and p1.ram = p2.ram and p1.model > p2.model
 
-17. Найдите модели ПК-блокнотов, скорость которых меньше скорости каждого из ПК.
+-- 17. Найдите модели ПК-блокнотов, скорость которых меньше скорости каждого из ПК.
 Вывести: type, model, speed
 select distinct product.type, laptop.model, laptop.speed from laptop
 join product on product.model= laptop.model
 where laptop.speed < all(select speed from pc)
 
-18. Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
+-- 18. Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
 select distinct maker, price from printer
 join product on product.model = printer.model
 where color = 'y'
 and
 price = (select min(price) from printer where color = 'y')
 
-19. Для каждого производителя, имеющего модели в таблице Laptop, найдите средний размер экрана выпускаемых им ПК-блокнотов.
+-- 19. Для каждого производителя, имеющего модели в таблице Laptop, найдите средний размер экрана выпускаемых им ПК-блокнотов.
 Вывести: maker, средний размер экрана.
 Select maker, avg(screen) from laptop
 join product on product.model = laptop.model
 group by maker
 
-20. Найдите производителей, выпускающих по меньшей мере три различных модели ПК. Вывести: Maker, число моделей ПК.
+-- 20. Найдите производителей, выпускающих по меньшей мере три различных модели ПК. Вывести: Maker, число моделей ПК.
 select maker, count(model) from product
 where type = 'pc'
 group by maker
 having count(model) >= 3
 
-21. Найдите максимальную цену ПК, выпускаемых каждым производителем, у которого есть модели в таблице PC.
+-- 21. Найдите максимальную цену ПК, выпускаемых каждым производителем, у которого есть модели в таблице PC.
 Вывести: maker, максимальная цена.
 Select maker, max(price) from product
 join pc on pc.model = product.model
 group by maker
 
-22. Для каждого значения скорости ПК, превышающего 600 МГц, определите среднюю цену ПК с такой же скоростью. Вывести: speed, средняя цена
+-- 22. Для каждого значения скорости ПК, превышающего 600 МГц, определите среднюю цену ПК с такой же скоростью. Вывести: speed, средняя цена
 Select speed, avg(price) from pc
 where speed > 600
 group by speed
 
-23. Найдите производителей, которые производили бы как ПК
+-- 23. Найдите производителей, которые производили бы как ПК
 со скоростью не менее 750 МГц, так и ПК-блокноты со скоростью не менее 750 МГц.
 Вывести: Maker
 select distinct maker from product
@@ -122,7 +122,7 @@ where speed >= 750 and maker in (select maker from product
 join laptop on laptop.model = product.model
 where speed >= 750)
 
-24. Перечислите номера моделей любых типов, имеющих самую высокую цену по всей имеющейся в базе данных продукции.
+-- 24. Перечислите номера моделей любых типов, имеющих самую высокую цену по всей имеющейся в базе данных продукции.
 select model from 
 (select model, price from pc
 union select model, price from printer
--- hw2_sql.sql
+++ hw2_sql.sql
@@ -1,17 +1,98 @@
+-- 1. Создать таблицу employees
+-- - id. serial,  primary key,
+-- - employee_name. Varchar(50), not null
+
 create table employees(
 id serial primary key,
 employee_name varchar(50) not null
 );
 
-insert into employees (employee_name)
-values ('Louise Sharp'),('Harold McCoy'),('Mae Rodriguez'),('Fred Clark'),('Debbie Rhodes'),('Juan Gill'),('Deborah Cox'),('Jacqueline Johnson'),('Ashley Sutton'),('Jim Howard'),('Charles Bryant'),('Robert Morris'),('Angela Hamilton'),('Sherry Wise'),('William Lewis'),('Richard Jones'),('Samantha Thomas'),('Kathy Stewart'),('Clayton Mills'),('Richard Reed'),('Douglas Powell'),('Peggy Lewis'),('Roger Glover'),('Ella Page'),('Terry Jenkins'),('Salvador Mason'),('Eleanor Webb'),('Christopher Brown'),('Eric Ford'),('Sonia Hayes'),('Jason Long'),('Rebecca Schultz'),('David Kelly'),('Marilyn Young'),('Tina Newman'),('Wanda Hayes'),('Monica Adams'),('Daniel Robinson'),('Christopher Wright'),('Dennis Jones'),('Chester Johnson'),('Joshua Watson'),('Louis Gutierrez'),('Robert Evans'),('Donna Warren'),('Alice Scott'),('Kristina Williams'),('Rachel Hill'),('Gregory Patterson'),('James Watkins'),('Gary Jones'),('Sheila Copeland'),('Danny McKinney'),('Beth Gardner'),('Alan Walters'),('Tara Wilson'),('Joyce Armstrong'),('Louis Young'),('David McGee'),('Mary Simpson'),('Cody Bishop'),('Andrew Reed'),('Amanda Kelly'),('Laura Pena'),('Nathan Lewis'),('Arthur Taylor'),('Theresa Mason'),('Michael Taylor'),('Brian Ellis'),('Eileen Williams');
+-- 2. Наполнить таблицу employee 70 строками
 
-create table salary_1(
+insert into employees (employee_name)
+values ('Louise Sharp'),
+('Harold McCoy'),
+('Mae Rodriguez'),
+('Fred Clark'),
+('Debbie Rhodes'),
+('Juan Gill'),
+('Deborah Cox'),
+('Jacqueline Johnson'),
+('Ashley Sutton'),
+('Jim Howard'),
+('Charles Bryant'),
+('Robert Morris'),
+('Angela Hamilton'),
+('Sherry Wise'),
+('William Lewis'),
+('Richard Jones'),
+('Samantha Thomas'),
+('Kathy Stewart'),
+('Clayton Mills'),
+('Richard Reed'),
+('Douglas Powell'),
+('Peggy Lewis'),
+('Roger Glover'),
+('Ella Page'),
+('Terry Jenkins'),
+('Salvador Mason'),
+('Eleanor Webb'),
+('Christopher Brown'),
+('Eric Ford'),
+('Sonia Hayes'),
+('Jason Long'),
+('Rebecca Schultz'),
+('David Kelly'),
+('Marilyn Young'),
+('Tina Newman'),
+('Wanda Hayes'),
+('Monica Adams'),
+('Daniel Robinson'),
+('Christopher Wright'),
+('Dennis Jones'),
+('Chester Johnson'),
+('Joshua Watson'),
+('Louis Gutierrez'),
+('Robert Evans'),
+('Donna Warren'),
+('Alice Scott'),
+('Kristina Williams'),
+('Rachel Hill'),
+('Gregory Patterson'),
+('James Watkins'),
+('Gary Jones'),
+('Sheila Copeland'),
+('Danny McKinney'),
+('Beth Gardner'),
+('Alan Walters'),
+('Tara Wilson'),
+('Joyce Armstrong'),
+('Louis Young'),
+('David McGee'),
+('Mary Simpson'),
+('Cody Bishop'),
+('Andrew Reed'),
+('Amanda Kelly'),
+('Laura Pena'),
+('Nathan Lewis'),
+('Arthur Taylor'),
+('Theresa Mason'),
+('Michael Taylor'),
+('Brian Ellis'),
+('Eileen Williams');
+
+-- 3. Создать таблицу salary
+-- - id. Serial  primary key,
+-- - monthly_salary. Int, not null
+
+create table salary(
 id serial primary key,
 monthly_salary int not null
 );
 
-insert into salary_1 (monthly_salary)
+-- 4. Наполнить таблицу salary 15 строками
+
+insert into salary (monthly_salary)
 values (1000),
 (1100),
 (1200),
@@ -29,11 +110,19 @@ values (1000),
 (2400),
 (2500);
 
+-- 5. Создать таблицу employee_salary
+-- - id. Serial  primary key,
+-- - employee_id. Int, not null, unique
+-- - salary_id. Int, not null
+
 create table employee_salary(
 id serial primary key,
 employee_id int not null unique,
 salary_id int not null
 );
+
+-- 6. Наполнить таблицу employee_salary 40 строками:
+-- - в 10 строк из 40 вставить несуществующие employee_id
 
 insert into employee_salary (employee_id, salary_id)
 values (3, 1),
@@ -77,14 +166,22 @@ values (3, 1),
 (91, 2),
 (4, 7);
 
-create table roles_1(
+-- 7. Создать таблицу roles
+-- - id. Serial  primary key,
+-- - role_name. int, not null, unique
+
+create table roles(
 id serial primary key,
 role_name int not null unique
 );
 
-alter table roles_1 ALTER COLUMN role_name type varchar(30)
+-- 8. Поменять тип столба role_name с int на varchar(30)
 
-insert into roles_1 (role_name)
+alter table roles ALTER COLUMN role_name type varchar(30);
+
+-- 9. Наполнить таблицу roles 20 строками
+
+insert into roles (role_name)
 values
 ('Junior Python developer'),
 ('Middle Python developer'),
@@ -107,6 +204,11 @@ values
 ('Middle Automation QA engineer'),
 ('Senior Automation QA engineer');
 
+-- 10. Создать таблицу roles_employee
+-- - id. Serial  primary key,
+-- - employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+-- - role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+
 CREATE TABLE roles_employee(
 id serial primary key,
 employee_id int not null unique,
@@ -115,11 +217,13 @@ role_id int not null,
 foreign key (role_id) references roles (id)
 );
 
+-- 11. Наполнить таблицу roles_employee 40 строками
+
 insert into roles_employee (employee_id, role_id)
-values (3, 3),
+values (3, 14),
 (52, 12),
 (14, 1),
-(8, 1),
+(8, 20),
 (9, 10),
 (11, 2),
 (36, 7),
@@ -129,11 +233,11 @@ values (3, 3),
 (59, 4),
 (66, 5),
 (2, 6),
-(23, 7),
+(23, 19),
 (29, 6),
 (17, 2),
 (25, 13),
-(26, 8),
+(26, 15),
 (30, 8),
 (18, 11),
 (6, 1),
@@ -149,12 +253,12 @@ values (3, 3),
 (57, 2),
 (37, 4),
 (49, 5),
-(7, 11),
+(7, 16),
 (67, 14),
-(42, 14),
+(42, 17),
 (51, 3),
 (63, 9),
-(43, 12),
+(43, 18),
 (4, 7);
 
 
